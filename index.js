3747eed68feed135f15131a8c5381063fcac342e
Gary Anderson <ganderson@seattleacademy.org>
2017-05-11 10:56:27 -0700

    draw all bots at once

--- index.js
+++ index.js
@@ -8,32 +8,26 @@ app.use(express.static(__dirname + '/public'));
 var bots = [];
 
 
-function botStatus(data) {
-    var myColor = data.color;
-    if (bots.length > 0) {
-        for (var i = bots.length; i > 0; i--) {
-            if (myColor == bots[i-1].color) {
-                bots.splice(i-1);
-            }
-        }
-    }
-    bots.push(data);
-    console.log("bots after push",bots);
-}
-
 function onConnection(socket) {
     function drawAllBots() {
-    	socket.broadcast.emit('clear', "");
-        for (i = 0; i < bots.length; i++) {
-            socket.broadcast.emit('drawing', bots[i]);
-        }
+        socket.emit('drawAllBots', bots);
     }
 
-    function onDrawing(data) {
-        botStatus(data);
+    function onPostBot(bot) {
+        if (bots.length > 0) { //Remove bot of same color
+            for (var i = bots.length; i > 0; i--) {
+                if (bot.color == bots[i - 1].color) {
+                    bots.splice(i - 1);
+                }
+            }
+        }
+        bots.push(bot);
         drawAllBots();
     }
-    socket.on('drawing', onDrawing);
+
+    socket.on('postbot', onPostBot);
+
+    drawAllBots();
 }
 
 io.on('connection', onConnection);
